05964e005c0e84350ae0d0f7bd4a62253ab981f8
Arun Chaudhary <69865631+Arun6552@users.noreply.github.com>
2023-11-06 09:59:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask,request,jsonify
-
+from thefuzz import fuzz
+import jaro
 from polyfuzz import PolyFuzz
-# request.data
+from Levenshtein import ratio
+
 
 app = Flask(__name__)
 polyfuzz = PolyFuzz()
 
 @app.route('/')
 def home ():
-    return "Welcome to Fuzzy Logic API created by Arun"
+    return "Welcome to Fuzzy Logic API"
 
 @app.route('/Fuzzylogic', methods = ['POST'])
 def FuzzyStringCheck():
@@ -17,7 +19,6 @@ def FuzzyStringCheck():
             request_data = request.get_json()
             fromstring = str(request_data.get("fromString"))
             toString = str(request_data.get("toString"))
-            modelNumber = str(request_data.get("modelNumber"))
 
             fromstring = fromstring.lower().strip()
             toString = toString.lower().strip()
@@ -34,11 +35,20 @@ def FuzzyStringCheck():
                 model1 = PolyFuzz("TF-IDF")
                 model1.match(from_list, to_list)
                 result1 = model1.get_matches()["Similarity"].values[0]
+                print(result1)
                 
                 model2 = PolyFuzz("EditDistance") 
                 model2.match(from_list, to_list)
                 result2 = model2.get_matches()["Similarity"].values[0]
-                return  jsonify({"Model_TFIDF":result1,"Model_EditDistance":result2})
+                print(result2)
+
+                result3 = fuzz.ratio(fromstring, toString)
+                result4 = fuzz.token_sort_ratio(fromstring, toString)
+                result5 = jaro.jaro_winkler_metric(fromstring,toString)
+                result6 = ratio(fromstring,toString)
+
+
+                return  jsonify({"Model_TFIDF_Percentage":result1,"Model_EditDistance_Percentage":result2,"Model_Ratio_Percentage":result3,"Model_TokenSort_Percentage":result4,"Model_jaroDistance_Percentage":result5,"Model_LevisteniDistance_Percentage":result6})
             
         except Exception as e:
             return "Interval Server Error :" + str(e),500
